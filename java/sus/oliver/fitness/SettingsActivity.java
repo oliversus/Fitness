35f81994a3f2e7cd72a912d42883ac602f58c83e
Oliver Sus <osus79@gmail.com>
2016-08-12 23:17:02 +0200

    now saving state of nighMode checkbox

--- java/sus/oliver/fitness/SettingsActivity.java
+++ java/sus/oliver/fitness/SettingsActivity.java
@@ -1,5 +1,7 @@
 package sus.oliver.fitness;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,25 +9,38 @@ import android.widget.CheckBox;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    public static CheckBox nightMode;
+    public static boolean checked;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        CheckBox nightMode = (CheckBox) findViewById(R.id.nightModeBox);
+        nightMode = (CheckBox) findViewById(R.id.nightModeBox);
         setDark(nightMode);
 
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        save(nightMode.isChecked());
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
+        nightMode.setChecked(load());
+        setDark(nightMode);
+
     }
 
     public void setDark(View view){
 
-        boolean checked = ((CheckBox) view).isChecked();
+        checked = ((CheckBox) view).isChecked();
 
         if (checked) {
             MainActivity.exerciseIsNight = true;
@@ -33,5 +48,17 @@ public class SettingsActivity extends AppCompatActivity {
             MainActivity.exerciseIsNight = false;
         }
 
+    }
+
+    private void save(final boolean isChecked) {
+        SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("check", isChecked);
+        editor.apply();
+    }
+
+    private boolean load() {
+        SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+        return sharedPreferences.getBoolean("check", false);
     }
 }
